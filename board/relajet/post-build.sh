737b96064411a1ad738a8121635f2e903d5855d7
Wig Cheng <wig.cheng@relajet.com>
2018-04-20 05:02:01 -0400

    c8328: add relaject evk boards support
    including initial script in rootfs_overlay folder.

---
+++ board/relajet/post-build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# post-build.sh for Pistachio SBC
+# 2017, Wig Cheng <onlywig@gmail.com>
+
+BOARD_DIR="$(dirname $0)"
+
+install -m 0644 -D ${BOARD_DIR}/rootfs_overlay/etc/inittab ${TARGET_DIR}/etc/inittab
+install -m 0644 -D ${BOARD_DIR}/rootfs_overlay/usr/bin/system_init ${TARGET_DIR}/usr/bin/system_init
