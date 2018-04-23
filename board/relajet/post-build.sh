8d6c4450cd8adb2970e3bbeae86376f0c73d3dbe
Wig Cheng <wig.cheng@relajet.com>
2018-04-23 05:50:25 -0400

    c8328: add libatomic to rootfs_overlay
    it's copy from arm-buildroot-linux-uclibcgnueabi 4.8.3 toolchain.

--- board/relajet/post-build.sh
+++ board/relajet/post-build.sh
@@ -6,3 +6,5 @@ BOARD_DIR="$(dirname $0)"
 
 install -m 777 -D ${BOARD_DIR}/rootfs_overlay/etc/inittab ${TARGET_DIR}/etc/inittab
 install -m 777 -D ${BOARD_DIR}/rootfs_overlay/usr/bin/system_init ${TARGET_DIR}/usr/bin/system_init
+install -m 777 -D ${BOARD_DIR}/rootfs_overlay/usr/lib/libatomic.so.1 ${TARGET_DIR}/usr/lib/libatomic.so.1
+install -m 777 -D ${BOARD_DIR}/rootfs_overlay/usr/lib/libatomic.so.1.0.0 ${TARGET_DIR}/usr/lib/libatomic.so.1.0.0
